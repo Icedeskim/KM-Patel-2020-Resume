e4d388cbbc8ead8d9ccd5b30c31cda9f542020f0
Kim Patel <kimmbie@gmail.com>
2020-04-30 22:24:45 -0600

    Java popup welcome text
    works but took me 8+ hours to figure out.

--- myJavaScript.js
+++ myJavaScript.js
@@ -2,10 +2,11 @@
 function greetUser()  {
     let name = document.getElementById('greetingName').value;
     document.getElementById('userName').innerHTML = name;
-    $('#modal-body').modal('hide');
-    $('#getUserName').modal('show');
+    $('#getUserName').modal('hide');
+    $('#greetUser').modal('show');
+   
 }
 $(document).ready(() => {
-    $('#greetUser').modal('show');
+    $('#getUserName').modal('show');
 });
 
